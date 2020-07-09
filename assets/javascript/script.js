f31bc0598935a5ea7aa347b26694630414feb149
kbetlach <kbetlach@gmail.com>
2020-07-09 13:22:41 -0500

    roll results now appear on the page. logic written for combining dice quantity, modifier and roll results. still need logic for multiple dice. small style updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,7 @@
+let resultsDiv = document.querySelector("#results");
+let diceQuantity = document.querySelector("#quantity");
+let diceModifier = document.querySelector("#modifier");
+
 let d4 = [1, 2, 3, 4];
 let d6 = [1, 2, 3, 4, 5, 6];
 let d8  = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -7,30 +11,30 @@ let d20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
 function rollD4() {
   let d4Result = d4[Math.floor(Math.random() * d4.length)];
-  console.log(d4Result);
+  resultsDiv.innerHTML = diceQuantity.value + " + " + diceModifier.value + " + " + d4Result;
 }
 
 function rollD6() {
     let d6Result = d6[Math.floor(Math.random() * d6.length)];
-    console.log(d6Result);
+    resultsDiv.innerHTML = d6Result;
   }
 
 function rollD8() {
     let d8Result = d8[Math.floor(Math.random() * d8.length)];
-    console.log(d8Result);
+    resultsDiv.innerHTML = d8Result;
   }
 
 function rollD10() {
     let d10Result = d10[Math.floor(Math.random() * d10.length)];
-    console.log(d10Result);
+    resultsDiv.innerHTML = d10Result;
   }
 
 function rollD12() {
     let d12Result = d12[Math.floor(Math.random() * d12.length)];
-    console.log(d12Result);
+    resultsDiv.innerHTML = d12Result;
   }
 
 function rollD20() {
     let d20Result = d20[Math.floor(Math.random() * d20.length)];
-    console.log(d20Result);
+    resultsDiv.innerHTML = d20Result;
   }
