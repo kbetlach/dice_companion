c877ff0f14c33e1fa358a0c127696b0b1fbfdf2f
kbetlach <kbetlach@gmail.com>
2020-08-22 09:10:47 -0500

    restructuring carousel javascript

--- assets/javascript/carousel.js
+++ assets/javascript/carousel.js
@@ -1,3 +1,12 @@
-function changeBackground() {
-  document.body.style.backgroundImage = "url('1.jpg')";
+function setBackground() {
+  document.body.style.backgroundImage = "url('assets/images/backgrounds/1.jpg')";
 }
+
+function changeBackground() {
+  let backgrounds = ['../images/backgrounds/1.jpg', 'assets/images/backgrounds/2.jpg', 'assets/images/backgrounds/3.jpg', 'assets/images/backgrounds/4.jpg', 'assets/images/backgrounds/5.jpg']
+  let newBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
+  document.body.style.backgroundImage = newBackground
+}
+
+setBackground();
